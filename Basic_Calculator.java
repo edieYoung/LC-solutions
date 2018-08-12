83eab0d2625b210e4224fe9db6521c95f27d065c
edieyoung <370676740@qq.com>
2018-08-11 20:54:36 -0500

    227

--- Basic_Calculator.java
+++ Basic_Calculator.java
@@ -7,54 +7,46 @@ class Solution {
     
     public int calculate(String s) {
        int ans = 0;
-    	int prev = 0;
-    	int num = 0;
-    	char op = '+';
-    	for(int i = 0;i<s.length();i++){
-    		char c = s.charAt(i);
-    		if(c==' '){
-    			continue;
-    		}
-    		if(Character.isDigit(c)){
-    			num = c - '0';
-    			
-    			System.out.println("aa");
-                if(i==s.length()-1){
-                    
-                }else{
-                    while(Character.isDigit(s.charAt(i+1))){
-    				    num = num*10 + (s.charAt(i+1) - '0');
-    				    if(i+1==s.length()-1){
-                            i++;
-                            break;
-                        }else{
-                            i++;
-                        }
-    			    }
-                }
+        int prev = 0;
+        int num = 0;
+        char op = '+';
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c==' '){
+                continue;
+            }
+            if(Character.isDigit(c)){
+                num = c - '0';
+                
+                
+                    while(i!=s.length()-1&&Character.isDigit(s.charAt(i+1))){
+                        num = num*10 + (s.charAt(i+1) - '0');
+                        i++;
+                    }
+                
             
-    			if(op=='+'){
-    				ans = ans+num;
-    				prev = num;
-    			}
-    			if(op=='-'){
-    				ans = ans-num;
-    				prev = -num;
-    			}
-    			if(op=='*'){
-    				ans = ans-prev+prev*num;
+                if(op=='+'){
+                    ans = ans+num;
+                    prev = num;
+                }
+                if(op=='-'){
+                    ans = ans-num;
+                    prev = -num;
+                }
+                if(op=='*'){
+                    ans = ans-prev+prev*num;
                     prev = prev*num;
-    			}
-    			if(op=='/'){
-    				ans = ans-prev+prev/num;
+                }
+                if(op=='/'){
+                    ans = ans-prev+prev/num;
                     prev = prev/num;
-    			}
-    		}else{
-    			op = c;
-    		}
+                }
+            }else{
+                op = c;
+            }
 
-    	}
-    	return ans;
+        }
+        return ans;
         }
     
 }
